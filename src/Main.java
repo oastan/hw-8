b8f5326e0579732b3b8101bb493e6c2e9efd03ce
Oleg <oastan@ya.ru>
2024-07-14 09:03:05 +0300

    hw1.1

--- src/Main.java
+++ src/Main.java
@@ -55,51 +55,50 @@ public class Main {
             total2 *= 1.07;
             if (month2 % 6 == 0) {
                 System.out.println("В месяце " + month2 + "сумма накоплений равна " + month2);
-
-                System.out.println("Задача 6");
-
-                int deposit3 = 15000;
-                int total3 = 0;
-                int year2 = 9;
-                int month3 = 1;
-                int monthInYear = 12;
-                int monthTotal = year2 * monthInYear;
-
-                while (month3 <= monthTotal ) {
-                    total3 = total3 + deposit3;
-                    total3 = total3 + (total3 * 7) / 100;
-                    if (month2 % 6 == 0)
-                    {
-                        System.out.println( month3 + " сумма накоплений равна " + total3);
-                    }
-                    month3++;
-
-
-
-
-                System.out.println();
-                System.out.println("Задача 7");
-
-                int firstFriday = 1;
-                int day = 0;
-                int daysPerMonth = 31;
-                for (day = firstFriday; day <= daysPerMonth; day += 7) {
-                    System.out.println("Сегодня пятница, " + day + "-е число. Необходимо подготовить отчет");
-
-                    System.out.println();
-                    System.out.println("Задача 8");
-                    int yearBefore = 2024 - 200;
-                    int yearAfter = 2024 + 100;
-                    int year1 = 0;
-                    for (year1 = yearBefore; year1 < yearAfter; year1++) {
-                        if (year1 % 79 == 0) {
-                            System.out.println(year1);
-                        }
-                    }
-
-                    }
-
-                }
             }
         }
+
+        System.out.println("Задача 6");
+
+        int deposit3 = 15000;
+        int total3 = 0;
+        int year2 = 9;
+        int month3 = 1;
+        int monthInYear = 12;
+        int monthTotal = year2 * monthInYear;
+
+        while (month3 <= monthTotal) {
+            total3 = total3 + deposit3;
+            total3 = total3 + (total3 * 7) / 100;
+            if (month3 % 6 == 0) {
+                System.out.println(month3 + " сумма накоплений равна " + total3);
+            }
+            month3++;
+
+
+            System.out.println();
+            System.out.println("Задача 7");
+
+            int firstFriday = 1;
+            int day = 0;
+            int daysPerMonth = 31;
+            for (day = firstFriday; day <= daysPerMonth; day += 7) {
+                System.out.println("Сегодня пятница, " + day + "-е число. Необходимо подготовить отчет");
+
+                System.out.println();
+                System.out.println("Задача 8");
+                int yearBefore = 2024 - 200;
+                int yearAfter = 2024 + 100;
+                int year1 = 0;
+                for (year1 = yearBefore; year1 < yearAfter; year1++) {
+                    if (year1 % 79 == 0) {
+                        System.out.println(year1);
+                    }
+                }
+
+            }
+
+        }
+
+
     }}
